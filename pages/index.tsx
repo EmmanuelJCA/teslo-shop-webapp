8f8fafeb2e52284721f5d0edc7f9cacd3844abdf
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-08-18 18:41:54 -0400

    Images grid in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '@/database/products';
 
 export default function Home() {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Los mejores productos de teslo aqui'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }} component='h1'>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={ product.title }
+                  >
+
+                  </CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
