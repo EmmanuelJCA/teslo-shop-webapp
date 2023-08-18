fb4711b84c1d7b88b7f20dab4b3f5394ea33cefe
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-08-17 22:54:31 -0400

    Shop layout: Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { Typography } from '@mui/material';
+import { ShopLayout } from '../components/layouts';
 
 export default function Home() {
   return (
-    <>
-      <h1>Teslo shop</h1>
-    </>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Los mejores productos de teslo aqui'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }} component='h1'>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
