ecfbcd91ab3bd8a0be3e254604299e027d9936ee
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-08-19 15:51:36 -0400

    slug page

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { FC, useMemo, useState } from 'react';
-import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import NextLink from 'next/link'
+import { Box, Card, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material';
 
 import { IProduct } from '../../interfaces';
 
@@ -25,15 +26,23 @@ export const ProductCard:FC<Props> = ({ product }) => {
       onMouseLeave={ () => setIsHovered(false) }
     >
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component='img'
-            className='fadeIn'
-            image={ productImage }
-            alt={ product.title }
-          >
-          </CardMedia>
-        </CardActionArea>
+        <NextLink 
+          href='/product/slug'
+          prefetch={ false }
+          legacyBehavior
+        >
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                className='fadeIn'
+                image={ productImage }
+                alt={ product.title }
+              >
+              </CardMedia>
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
       <Box sx={{ mt:1 }} className='fadeIn'>
